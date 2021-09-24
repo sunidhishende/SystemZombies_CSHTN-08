5d780627aa3a908b67eaee746eb006875fb06812
Sunidhi Shende <sunidhi.shende1@gmail.com>
2021-09-24 23:00:35 +0530

    Added controls to video

--- systemZombies/wsgi.py
+++ systemZombies/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+from static_ranges import Ranges
+from dj_static import Cling, MediaCling
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'systemZombies.settings')
 
-application = get_wsgi_application()
+
+application = Ranges(Cling(MediaCling(get_wsgi_application())))   
